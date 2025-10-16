907438f5791506998a5dd20faeba0ac2adb09140
Marvin Schmidt <marvin.schmidt@active-value.de>
2025-10-16 18:53:16 +0200

    added typing effect partial

---
+++ src/scripts/partials/typingEffect.js
@@ -0,0 +1,36 @@
+export const typingEffect = {
+    vars: {
+        queries: {
+            element: '*[data-typing-effect]'
+        },
+        minDelay: 50,
+        maxDelay: 250
+    },
+
+    init() {
+        const elements = document.querySelectorAll(this.vars.queries.element);
+        elements.forEach(element => this.startTypingEffect(element));
+    },
+
+    find(){
+        const $elements = document.querySelectorAll(this.vars.queries.element);
+        $elements.forEach($element => this.startTypingEffect($element));
+    },
+
+    startTypingEffect($element) {
+        const text = $element.textContent;
+        $element.textContent = '';
+        let currentChar = 0;
+
+        const type = () => {
+            if (currentChar < text.length) {
+                $element.textContent += text[currentChar];
+                currentChar++;
+                const randomDelay = Math.floor(Math.random() * (this.vars.maxDelay - this.vars.minDelay + 1)) + this.vars.minDelay;
+                setTimeout(type, randomDelay);
+            }
+        };
+
+        setTimeout(type, 200);
+    }
+};
