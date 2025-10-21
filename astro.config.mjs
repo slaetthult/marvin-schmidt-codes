1447c1256d3bbfb96a8ed7ace6422bbb1a6b8de9
Marvin Schmidt <marvin.schmidt@active-value.de>
2025-10-21 15:13:26 +0200

    updated dpr

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
             entrypoint: "./src/scripts/utils/cloudinary-image-service.ts",
             config: {
                 cloudName: process.env.CLOUDINARY_CLOUD_NAME || import.meta.env.CLOUDINARY_CLOUD_NAME,
-                retinaDPRs: [2],
+                retinaDPRs: [1.75],
                 baseTransforms: ["f_auto", "q_auto"],
                 maxWidth: 2400
             }
